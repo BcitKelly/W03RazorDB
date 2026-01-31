7053144b86dc7c461e3c2636eb0deadda5478bf0
BcitKelly <72475775+BcitKelly@users.noreply.github.com>
2026-01-30 21:21:49 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using W03RazorDB.Models.CompanyDB;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("CompanyConnection") ?? throw new InvalidOperationException("Connection string 'CompanyContext' not found.");
+
+builder.Services.AddDbContext<CompanyContext>(options => options.UseSqlite(connectionString));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
